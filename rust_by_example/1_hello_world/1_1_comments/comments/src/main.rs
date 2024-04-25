d7b67ddd632cbf8920d1356617b06143066efa97
APRust <alex.pikalov.rust@gmail.com>
2024-04-25 16:22:36 +0300

    add rbe/1_1_comments

--- rust_by_example/1_hello_world/1_1_comments/comments/src/main.rs
+++ rust_by_example/1_hello_world/1_1_comments/comments/src/main.rs
@@ -1,5 +1,18 @@
-fn main() {
-    println!("test");
+//! # main()
+//! Its Doc comment for enclosing item for main() function
 
+/// Doc comment
+///
+// Line comment
+
+/*
+* Block comment
+* for test doc comment run 'cargo doc --open', 'cargo doc' for only generation
+*
+* for selecting and paste into Block comment Option+Command+/
+*
+*/
+fn main() {
+    /*println!("test");*/
     println!("Hello, world!");
 }
