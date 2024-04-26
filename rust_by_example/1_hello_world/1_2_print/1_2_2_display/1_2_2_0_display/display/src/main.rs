c3d6ab34e1092af0dfd2c36608c34547009e339c
APRust <alex.pikalov.rust@gmail.com>
2024-04-26 19:33:07 +0300

    reb 1_2_2_0_display

--- rust_by_example/1_hello_world/1_2_print/1_2_2_display/1_2_2_0_display/display/src/main.rs
+++ rust_by_example/1_hello_world/1_2_print/1_2_2_display/1_2_2_0_display/display/src/main.rs
@@ -1,3 +1,22 @@
+//Import (via 'use') the 'fmt' module to make it available.
+use std::fmt;
+use std::fmt::Formatter;
+
+#[derive(Debug)]
+struct Structure(i32);
+
+//To use the '{}' marker, the trait 'fmt::Display' must be implemented
+impl fmt::Display for Structure {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        write!(f, "self.0 = {}", self.0)
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    //Print with Display'{}'
+    println!("Display print is: {}", Structure(23));
+
+    //Print with Debug
+    println!("Debug print is: {:?}", Structure(23));
+    println!("Pretty Debug print is: {:#?}", Structure(23));
 }
