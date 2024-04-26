cb088a26bbc257015702fc0eab29f403f4ee65c3
APRust <alex.pikalov.rust@gmail.com>
2024-04-26 19:15:02 +0300

    add reb/1_2_1_debug

--- rust_by_example/1_hello_world/1_2_print/1_2_1_debug/debug/src/main.rs
+++ rust_by_example/1_hello_world/1_2_print/1_2_1_debug/debug/src/main.rs
@@ -1,3 +1,27 @@
+// This structure cannot be printed either with 'fmt::Display' or with 'fmt::Debug'
+#[allow(dead_code)]
+struct UnPrintable(i32);
+
+//derive attribute automatically creates the implementation required to make this 'struct'
+//printable with 'fmt::Debug'
+#[derive(Debug)]
+struct Structure(i32);
+
+#[derive(Debug)]
+struct Deep(Structure);
+
 fn main() {
-    println!("Hello, world!");
+    // Print with {:?} is similar to with {}
+    println!("{:?} months in a year. ", 12);
+    println!(
+        "{1:?} {0:?} is the {actor:?} name.",
+        "Slater",
+        "Christian",
+        actor = "actor's"
+    );
+    // Standard fmt::Debug print
+    println!("Now {:?}, will print!", Deep(Structure(7)));
+
+    // Pretty print
+    println!("Now {:#?}, will print!", Deep(Structure(7)));
 }
